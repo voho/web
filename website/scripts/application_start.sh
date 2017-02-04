b98be07935247431ef12e8ce373146217d43d1bb
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2017-02-05 00:32:18 +0100

    new approach

--- website/scripts/application_start.sh
+++ website/scripts/application_start.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-STAGING="/tmp/staging"
-WAR="$STAGING/website.war"
-WEBAPPS=/usr/share/tomcat8/webapps
-
-rm -rf $WEBAPPS/ROOT $WEBAPPS/ROOT.war
-cp $WAR $WEBAPPS/ROOT.war
+# start Tomcat
 
 service tomcat8 start
