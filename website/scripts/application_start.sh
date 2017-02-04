a2c57abbd070e772892066a7b7b76770e3a12c87
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2017-02-05 00:20:49 +0100

    more debug

--- website/scripts/application_start.sh
+++ website/scripts/application_start.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-WAR=website.war
+WAR=./website.war
 WEBAPPS=/usr/share/tomcat8/webapps
 
-rm -rfv $WEBAPPS/ROOT
+rm -rf $WEBAPPS/ROOT
+rm -rf $WEBAPPS/ROOT.war
+pwd
+ls -l
 cp $WAR $WEBAPPS/ROOT.war
 
 service tomcat8 start
