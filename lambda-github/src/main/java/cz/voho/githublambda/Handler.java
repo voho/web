94fb1f6e28e049ecce64bd0a2f960a5c17b9b53a
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2017-03-14 23:50:21 +0100

    cleanup

--- lambda-github/src/main/java/cz/voho/githublambda/Handler.java
+++ lambda-github/src/main/java/cz/voho/githublambda/Handler.java
@@ -8,12 +8,18 @@ import com.jayway.jsonpath.ReadContext;
 import cz.voho.web.lambda.model.github.CommitMeta;
 import cz.voho.web.lambda.model.github.LatestCommitsRequest;
 import cz.voho.web.lambda.model.github.LatestCommitsResponse;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -29,9 +35,10 @@ import java.util.List;
  */
 public class Handler implements RequestHandler<LatestCommitsRequest, LatestCommitsResponse> {
     private static final String GET_REPO_EVENTS_URI_FORMAT = "https://api.github.com/repos/voho/website/commits?path=%s";
+    private static final HttpHost TARGET_HOST = new HttpHost("api.github.com", 443, "https");
     private static final String GITHUB_USER = System.getenv("GITHUB_USER");
     private static final String GITHUB_TOKEN = System.getenv("GITHUB_TOKEN");
-    private static final String GITHUB_AUTH = System.getenv("GITHUB_AUTH");
+    private static final UsernamePasswordCredentials CREDENTIALS = new UsernamePasswordCredentials(GITHUB_USER, GITHUB_TOKEN);
 
     @Override
     public LatestCommitsResponse handleRequest(final LatestCommitsRequest request, final Context context) {
@@ -50,20 +57,22 @@ public class Handler implements RequestHandler<LatestCommitsRequest, LatestCommi
         String url = String.format(GET_REPO_EVENTS_URI_FORMAT, request.getPath());
 
         try (CloseableHttpClient client = createHttpClient()) {
-            HttpResponse response = executeGetJsonRequest(context, url, client);
-            String json = readResponse(context, response);
-            ReadContext ctx = JsonPath.parse(json);
-            List<String> urls = ctx.read("$[*].url");
-            for (String u : urls) {
-                HttpResponse commitResponse = executeGetJsonRequest(context, u, client);
-                String json2 = readResponse(context, commitResponse);
-                ReadContext ctx2 = JsonPath.parse(json2);
-                CommitMeta cm = new CommitMeta();
-                cm.setIsoTime(ctx2.read("$.commit.author.date"));
-                cm.setMessage(ctx2.read("$.commit.message"));
-                cm.setFilenames(ctx2.read("$.files[*].filename"));
-                cm.setSha(ctx2.read("$.sha"));
-                result.add(cm);
+            HttpResponse outerResponse = executeGetJsonRequest(context, url, client);
+            String outerJson = readResponse(context, outerResponse);
+            ReadContext outerContext = JsonPath.parse(outerJson);
+            List<String> outerUrls = outerContext.read("$[*].url");
+
+            for (String innerUrl : outerUrls) {
+                HttpResponse innerResponse = executeGetJsonRequest(context, innerUrl, client);
+                String innerJson = readResponse(context, innerResponse);
+                ReadContext innerContext = JsonPath.parse(innerJson);
+
+                CommitMeta commitMeta = new CommitMeta();
+                commitMeta.setIsoTime(innerContext.read("$.commit.author.date"));
+                commitMeta.setMessage(innerContext.read("$.commit.message"));
+                commitMeta.setFilenames(innerContext.read("$.files[*].filename"));
+                commitMeta.setSha(innerContext.read("$.sha"));
+                result.add(commitMeta);
             }
         }
 
@@ -78,24 +87,43 @@ public class Handler implements RequestHandler<LatestCommitsRequest, LatestCommi
     }
 
     private HttpResponse executeGetJsonRequest(Context context, final String url, final CloseableHttpClient client) throws IOException {
-        HttpGet get = new HttpGet(url);
-        get.setHeader("Accept", "application/json");
-        get.setHeader("Authorization", GITHUB_AUTH);
-        return client.execute(get);
+        BasicScheme basicAuth = new BasicScheme();
+
+        AuthCache authCache = new BasicAuthCache();
+        authCache.put(TARGET_HOST, basicAuth);
+
+        HttpClientContext clientContext = HttpClientContext.create();
+        clientContext.setCredentialsProvider(createCredentialsProvider());
+        clientContext.setAuthCache(authCache);
+
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setHeader("Accept", "application/json");
+        return client.execute(httpGet, clientContext);
     }
 
     private void validateResponseOk(Context context, HttpResponse response) throws IOException {
         if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-            log(context, "HTTP status received: " + response.getStatusLine() + " USER: " + GITHUB_USER);
             throw new IOException("Invalid HTTP status: " + response.getStatusLine().getStatusCode());
         }
     }
 
+    private CredentialsProvider createCredentialsProvider() {
+        CredentialsProvider result = new BasicCredentialsProvider();
+        result.setCredentials(AuthScope.ANY, CREDENTIALS);
+        return result;
+    }
+
     private CloseableHttpClient createHttpClient() {
-        CredentialsProvider provider = new BasicCredentialsProvider();
-        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(GITHUB_USER, GITHUB_TOKEN);
-        provider.setCredentials(AuthScope.ANY, credentials);
-        return HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+        return HttpClientBuilder
+                .create()
+                .setDefaultRequestConfig(
+                        RequestConfig
+                                .custom()
+                                .setRedirectsEnabled(true)
+                                .setAuthenticationEnabled(true)
+                                .build()
+                )
+                .build();
     }
 
     private void log(Context context, String messageFormat, Object... arguments) {
