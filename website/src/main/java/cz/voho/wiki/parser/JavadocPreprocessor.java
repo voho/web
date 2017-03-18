19fe578e7019f0ddc7db7bdd4786f7bf90e41542
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2017-03-18 21:04:31 +0100

    added bold class name in javadoc macros

--- website/src/main/java/cz/voho/wiki/parser/JavadocPreprocessor.java
+++ website/src/main/java/cz/voho/wiki/parser/JavadocPreprocessor.java
@@ -1,10 +1,12 @@
 package cz.voho.wiki.parser;
 
+import com.google.common.base.Joiner;
 import com.google.common.net.UrlEscapers;
 import cz.voho.common.utility.ReplacePatternCallback;
-import cz.voho.wiki.repository.parsed.WikiParsingContext;
 import cz.voho.wiki.model.WikiPageSource;
+import cz.voho.wiki.repository.parsed.WikiParsingContext;
 
+import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 public class JavadocPreprocessor implements Preprocessor {
@@ -12,9 +14,21 @@ public class JavadocPreprocessor implements Preprocessor {
     public String preprocessSource(final WikiParsingContext context, final WikiPageSource wikiPageSource, final String source) {
         final ReplacePatternCallback rp = new ReplacePatternCallback(Pattern.compile("\\*javadoc:(.+?)\\*"));
         return rp.replace(source, matchResult -> {
-            final String className = matchResult.group(1);
+            final String className = processClassName(matchResult);
             final String classNameJavadocUrl = String.format("http://grepcode.com/search?query=%s", UrlEscapers.urlFragmentEscaper().escape(className));
             return String.format("[%s](%s)", className, classNameJavadocUrl);
         });
+    }
+
+    private String processClassName(MatchResult matchResult) {
+        String result = matchResult.group(1);
+        String[] path = result.split(Pattern.quote("."));
+
+        if (path.length > 0) {
+            path[path.length - 1] = String.format("**%s**", path[path.length - 1]);
+            result = Joiner.on(".").join(path);
+        }
+
+        return result;
     }
 }
