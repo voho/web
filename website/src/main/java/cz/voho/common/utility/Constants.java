401520d91b39db6e6e62c504ea6b65ff6fc8738b
Vojtěch Hordějčuk <voho@users.noreply.github.com>
2017-09-17 21:35:11 +0200

    updated Spotify profile

--- website/src/main/java/cz/voho/common/utility/Constants.java
+++ website/src/main/java/cz/voho/common/utility/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
     public static final String PROFILE_ITUNES = "https://itunes.apple.com/us/artist/vojtech-hordejcuk/id929462760";
     public static final String PROFILE_AMAZON = "https://www.amazon.com/s/ref=ntt_srch_drd_B00OHRU6TC?ie=UTF8&amp;field-keywords=Vojtech%20Hordejcuk&amp;index=digital-music&amp;search-type=ss";
     public static final String PROFILE_GOOGLE_MUSIC = "https://play.google.com/store/music/artist/Vojtech_Hordejcuk?id=Ajqelbv4qqwma7ewjtd6pkmkkie";
-    public static final String PROFILE_SPOTIFY = "https://open.spotify.com/artist/02pC7TpobLA5my9tbGgNyl";
+    public static final String PROFILE_SPOTIFY = "https://open.spotify.com/artist/4OaWtKAgl8oGU9QTa4wXu4";
     public static final String PROFILE_YOUTUBE = "https://www.youtube.com/channel/UCmhcw6dUD3J7dQz17l2KmCg";
     public static final String PROFILE_PAYPAL = "https://www.paypal.me/vojtisek";
     public static final Locale CZECH_LOCALE = new Locale("cs", "CZ");
