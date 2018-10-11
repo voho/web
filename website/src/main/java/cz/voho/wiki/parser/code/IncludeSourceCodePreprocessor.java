00ebbd26d507470e29ad999acce3c7097015ec45
Vojta Hordejcuk <vojta@amazon.com>
2018-10-11 14:26:18 +0200

    moving example zip location (attempt 1)

--- website/src/main/java/cz/voho/wiki/parser/code/IncludeSourceCodePreprocessor.java
+++ website/src/main/java/cz/voho/wiki/parser/code/IncludeSourceCodePreprocessor.java
@@ -21,7 +21,7 @@ public class IncludeSourceCodePreprocessor implements CodeProcessor {
     private static final String TRAVIS_BUILD_URL = "https://travis-ci.org/voho/web";
 
     private static final String INCLUDE_PREFIX = "include:";
-    private static final String EXAMPLES_ZIP_LOCATION = "/tmp/examples.zip";
+    private static final String EXAMPLES_ZIP_LOCATION = "examples.zip";
 
     @Override
     public boolean handle(final HtmlWriter html, final String codeLang, final String codeSource) {
